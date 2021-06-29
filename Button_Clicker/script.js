ac761b66399a21a01d553c763b640cddf2ce68ef
hope-crichlow <hopecrichlow@gmail.com>
2021-06-28 20:23:40 -0700

    Added like counter to one of the buttons

--- Button_Clicker/script.js
+++ Button_Clicker/script.js
@@ -11,3 +11,13 @@ function removeElement(element) {
 function likeAlert(button) {
     window.alert("Ninja was liked");
 }
+
+//Incrament Likes Count 
+var likes = 0;
+
+function addLike(button){
+    var givenLikes = document.getElementById("likes").innerText;
+    likes = givenLikes;
+    likes ++;
+    document.getElementById("likes").innerText = likes;
+}

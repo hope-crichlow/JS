2348c63f8cc3620ce07fde6a3f7eda39853c25a1
hope-crichlow <hopecrichlow@gmail.com>
2021-06-29 16:32:34 -0700

    HTML clean up

--- Likes/script.js
+++ Likes/script.js
@@ -1,14 +1,73 @@
 //Incrament Likes Count 
-var likes = 0;
+var likes1 = 0;
+var likes2 = 0;
+var likes3 = 0;
 
-function addLike(button){
-    console.log("Likes", likes);
-    var givenLikes = document.querySelector("#likes").innerText;
+function addLike1(button){
+    console.log("Likes1", likes1);
+    var givenLikes = document.querySelector("#likes1").innerText;
     console.log(givenLikes);
-    likes = givenLikes;
-    likes ++;
-    document.querySelector("#likes").innerText = likes;
+    likes1 = givenLikes;
+    likes1 ++;
+    document.querySelector("#likes1").innerText = likes1;
 }
+
+function addLike2(button){
+    console.log("Likes2", likes2);
+    var givenLikes = document.querySelector("#likes2").innerText;
+    console.log(givenLikes);
+    likes2 = givenLikes;
+    likes2 ++;
+    document.querySelector("#likes2").innerText = likes2;
+}
+
+function addLike3(button){
+    console.log("Likes3", likes3);
+    var givenLikes = document.querySelector("#likes3").innerText;
+    console.log(givenLikes);
+    likes3 = givenLikes;
+    likes3 ++;
+    document.querySelector("#likes3").innerText = likes3;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var card = {
 //     name: "Selim",
@@ -17,17 +76,17 @@ function addLike(button){
 // }
 // console.log (card);
 
-var feedDetail = [
-    {name: "Selim", likes: 4, comment:"Trolling is the name of the game" }
-]
-console.log ("no",feedDetail);
+// var feedDetail = [
+//     {name: "Selim", likes: 4, comment:"Trolling is the name of the game" }
+// ]
+// console.log ("no",feedDetail);
 
 
 
-function generateCards(){
-    console.log(document.getElementById("hello"));
+// function generateCards(){
+//     console.log(document.getElementById("hello"));
 
-    var cardList = document.getElementById("hello");
+//     var cardList = document.getElementById("hello");
     
     // document.getElementById("hello").innerHTML = feedDetail.map(card =>
     //     `<div>
@@ -37,10 +96,10 @@ function generateCards(){
     //     </div>`
     // ).join('')
 
-}
+// }
 
-generateCards();
-cardFactory(); 
+// generateCards();
+// cardFactory(); 
 
 
     // var feed = document.querySelector("#hello").innerText;
@@ -55,12 +114,12 @@ cardFactory();
 
 // var yourFeed = [];
 
-function cardFactory(name, likes, comment) {
-    var card = {};
-    card.name = name;
-    card.likes = likes;
-    card.comment = comment;
-    return card;
-}
+// function cardFactory(name, likes, comment) {
+//     var card = {};
+//     card.name = name;
+//     card.likes = likes;
+//     card.comment = comment;
+//     return card;
+// }
 
 
